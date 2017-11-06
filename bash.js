acab309569ba226d599e03fcfdf0c869587c8884
Albert Oh <albertopha@hotmail.com>
2017-11-07 01:44:36 +0900

    refactored node

--- bash.js
+++ bash.js
@@ -1,10 +1,20 @@
 var command = require('./command');
 // Output a prompt
+process.stdin.on('data', function (data) {
+    var str = data.toString().trim(); // remove the newline
+    if(str.indexOf(" ") !== -1){
+      var cmd = str.slice(0, str.indexOf(' '));
+      var file = str.slice(str.indexOf(' ')+1);
+    }else{
+      var cmd = str;
+    }
+    command[cmd] && command[cmd](file);
+});
 process.stdout.write('prompt > ');
-command.pwd();
-command.date();
-command.ls();
-command.echo();
+
+
+
+
 
 // The stdin 'data' event fires after a user types in a line
 
