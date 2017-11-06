e7180a8ba3ac1cc88eb0c85459b6755f5a6b2e48
Yan.Shen <sheny308@yahoo.com>
2017-11-06 11:04:40 -0500

    echo

--- bash.js
+++ bash.js
@@ -1,1 +1,10 @@
-// change
+var command = require('./command');
+// Output a prompt
+process.stdout.write('prompt > ');
+command.pwd();
+command.date();
+command.ls();
+command.echo();
+
+// The stdin 'data' event fires after a user types in a line
+
